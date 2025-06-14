3737fefff75f0199f1901dfef5396758dd6a6409
GaryJsN <garyjohnson1994@gmail.com>
2025-06-14 14:30:10 +0200

    1st commit

---
+++ meteo.js
@@ -0,0 +1,30 @@
+async function getConfig() {
+  const response = await fetch("conf.json");
+  const data = await response.json();
+  return data;
+}
+
+async function getMeteo(lat, lon) {
+  const url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current_weather=true&timezone=auto`;
+  const response = await fetch(url);
+  const data = await response.json();
+  return data.current_weather;
+}
+
+function afficherMeteo(meteo) {
+  const meteoDiv = document.getElementById("meteo");
+  let symbole = meteo.temperature > 15 ? "☀️" : "❄️";
+  if (meteoDiv) {
+    meteoDiv.innerHTML = `${symbole} ${meteo.temperature}°C<br>Vent : ${meteo.windspeed} km/h`;
+  }
+}
+
+async function majMeteo() {
+  const config = await getConfig();
+  const meteo = await getMeteo(config.latitude, config.longitude);
+  console.log("Données météo récupérées :", meteo);
+  afficherMeteo(meteo);
+}
+
+majMeteo();
+setInterval(majMeteo, 60 * 60 * 1000);
